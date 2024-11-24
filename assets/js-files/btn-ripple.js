cc6744dbac6e6964c5a4fa21db8bbf8526dbb6be
aditya tripathi <adityawebiwork@gmail.com>
2024-11-24 01:35:11 -0800

    add code

--- assets/js-files/btn-ripple.js
+++ assets/js-files/btn-ripple.js
@@ -9,7 +9,7 @@ function addRippleEffect(e) {
     ripple.style.width = ripple.style.height = `${size}px`;
     ripple.style.left = `${x}px`;
     ripple.style.top = `${y}px`;
-    ripple.classList.add("ripple");
+    ripple.classList.add("white_ripple");
 
     button.appendChild(ripple);
     ripple.addEventListener("animationend", () => {
@@ -17,7 +17,34 @@ function addRippleEffect(e) {
     });
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.btn_ripple_white');
 buttons.forEach(button => {
     button.addEventListener("click", addRippleEffect);
+});
+
+
+// btn_theme_ripple
+
+function addRippleEffect_2(e) {
+    const ripple = document.createElement("span");
+    const button = e.currentTarget;
+    const rect = button.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    const x = e.clientX - rect.left - size / 2;
+    const y = e.clientY - rect.top - size / 2;
+
+    ripple.style.width = ripple.style.height = `${size}px`;
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+    ripple.classList.add("theme_ripple");
+
+    button.appendChild(ripple);
+    ripple.addEventListener("animationend", () => {
+        ripple.remove();
+    });
+}
+
+const buttons_2 = document.querySelectorAll('.btn_theme_ripple');
+buttons_2.forEach(button => {
+    button.addEventListener("click", addRippleEffect_2);
 });
